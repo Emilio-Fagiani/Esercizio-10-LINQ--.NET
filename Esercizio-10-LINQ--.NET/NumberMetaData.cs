d34f24f852a76da8eef9ec502ee6565d61ca2ef0
emifa <emifa@LAPTOP-1TM449QB>
2023-07-09 14:32:59 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-10-LINQ--.NET/NumberMetaData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_10_LINQ__.NET
+{
+    internal class NumberMetaData
+    {
+        public bool IsEven { get; set; }
+        public int NumberOfCharacters { get; set; }
+    }
+}
---
+++ Esercizio-10-LINQ--.NET/Program.cs
@@ -0,0 +1,25 @@
+﻿using Esercizio_10_LINQ__;
+using Esercizio_10_LINQ__.NET;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+
+        List<int> numbers = Enumerable.Range(1, 100)
+            .Select(_ => random.Next(1, 1000))
+            .ToList();
+
+        List<NumberMetaData> metaDataList = numbers.Select(number => new NumberMetaData
+        {
+            IsEven = number % 2 == 0,
+            NumberOfCharacters = number.ToString().Length
+        }).ToList();
+
+        foreach (NumberMetaData metaData in metaDataList)
+        {
+            Console.WriteLine($"IsEven: {metaData.IsEven}, NumberOfCharacters: {metaData.NumberOfCharacters}");
+        }
+    }
+}
